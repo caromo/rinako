b673755d27e66429b1527142d2416d74bff37036
Carlo Romo <carlo.romo@protonmail.com>
2020-07-25 04:50:47 -0400

    Day2 check-out: Factorize code a bit

--- commands.go
+++ commands.go
@@ -26,13 +26,16 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		session: s,
 		message: m,
 	}
+	err := mEvent.enrich()
+	if err != nil {
+		log.Printf("Failed to enrich message event: %v", err)
+	}
 
 	if strings.HasPrefix(m.Content, discriminator) {
 		removeDisc := strings.Replace(m.Content, discriminator, "", 1)
 		command, args := removeHead(strings.Split(removeDisc, " "))
 
-		err := mEvent.processCommand(command, args)
-		if err != nil {
+		if err = mEvent.processCommand(command, args); err != nil {
 			log.Printf("Error executing %s with args %s: %s", command, args, err)
 		}
 	}
@@ -57,35 +60,49 @@ func (m *messageEvent) role(args []string) {
 	opt, role := removeHead(args)
 	fmt.Printf("args: %v\n", args)
 	toModify := toCleanRole(strings.Join(role, " "))
+
 	switch opt {
 	case "add":
 		toModifyRole, err := m.getRole(toModify)
 		if err != nil {
 			log.Printf("Error getting role matching %s", toModify)
 		}
-		fmt.Printf("Role add: Role %v", toModifyRole)
-		err = m.session.GuildMemberRoleAdd(m.message.GuildID, m.message.Author.ID, toModifyRole.ID)
+
+		err = m.session.GuildMemberRoleAdd(m.message.GuildID, m.member.User.ID, toModifyRole.ID)
 		if err != nil {
-			log.Printf("Error adding role %s on User %s", toModifyRole.Name, m.message.Author.Username)
-			m.session.ChannelMessageSend(m.message.ChannelID, "Failed to add role")
+			log.Printf("Error adding role %s on User %s", toModifyRole.Name, m.member.User.Username)
+
+			m.sendMessagef("Failed to add role %s", toModifyRole.Name)
 		}
-		m.session.ChannelMessageSend(m.message.ChannelID, "Added role")
+
+		m.sendMessagef("Added role %s", toModifyRole.Name)
+
 	case "remove":
 		toModifyRole, err := m.getRole(toModify)
 		if err != nil {
 			log.Printf("Error getting role matching %s", toModify)
 		}
-		err = m.session.GuildMemberRoleRemove(m.message.GuildID, m.message.Author.ID, toModifyRole.ID)
+
+		err = m.session.GuildMemberRoleRemove(m.message.GuildID, m.member.User.ID, toModifyRole.ID)
 		if err != nil {
-			log.Printf("Error adding role %s on User %s", toModifyRole.Name, m.message.Author.Username)
-			m.session.ChannelMessageSend(m.message.ChannelID, "Failed to remove role")
+			log.Printf("Error adding role %s on User %s", toModifyRole.Name, m.member.User.Username)
+			m.sendMessagef("Failed to remove role %s", toModifyRole.Name)
 		}
-		m.session.ChannelMessageSend(m.message.ChannelID, "Removed role")
+
+		m.sendMessagef("Removed role %s", toModifyRole.Name)
+
 	default:
-		m.session.ChannelMessageSend(m.message.ChannelID, "'role' command usage: `role [add/remove] \"role\"`")
+		m.sendMessagef("'role' command usage: `role [add/remove] \"role\"`")
 	}
 }
 
+func (m *messageEvent) sendMessage(text string) {
+	m.session.ChannelMessageSend(m.message.ChannelID, text)
+}
+
+func (m *messageEvent) sendMessagef(format string, a ...interface{}) {
+	m.session.ChannelMessageSend(m.message.ChannelID, fmt.Sprintf(format, a))
+}
 func (m *messageEvent) getRole(role string) (res *discordgo.Role, err error) {
 	var guild *discordgo.Guild
 	guild, err = m.session.Guild(m.message.GuildID)
@@ -106,10 +123,33 @@ func (m *messageEvent) getRole(role string) (res *discordgo.Role, err error) {
 }
 
 func (m *messageEvent) test() {
-	m.session.ChannelMessageSend(m.message.ChannelID, "璃奈ちゃんボード「ヤッタゼー！」")
+	m.sendMessage("璃奈ちゃんボード「ヤッタゼー！」")
+}
+
+func (m *messageEvent) enrich() (err error) {
+	m.guild, err = m.session.Guild(m.message.GuildID)
+	if err != nil {
+		log.Printf("Error fetching guild data: %v", err)
+		return
+	}
+	m.channel, err = m.session.Channel(m.message.ChannelID)
+	if err != nil {
+		log.Printf("Error fetching channel data: %v", err)
+		return
+	}
+	m.member, err = m.session.GuildMember(m.guild.ID, m.message.Author.ID)
+	if err != nil {
+		log.Printf("Error fetching member data: %v", err)
+		return
+	}
+	return
 }
 
 type messageEvent struct {
 	session *discordgo.Session
 	message *discordgo.MessageCreate
+
+	guild   *discordgo.Guild
+	channel *discordgo.Channel
+	member  *discordgo.Member
 }
